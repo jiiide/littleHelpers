d0691e75c49412a45422f57ab15396fc414ecdff
jiiide <j.d.alindogan@gmail.com>
2020-10-07 16:32:10 -0700

    Created an authorization function for Professors

--- lhWebApp/functions.ts
+++ lhWebApp/functions.ts
@@ -3,11 +3,18 @@ function addStudent(data) {
 }
 
 function getId(url: string){
- console.log(url.match(/[-\w]{25,}/)[0]) 
+ console.log(url.match(/[-\w]{25,}/)[0]);
 }
 
-function isAuthorized(email: string){
- var numbers = email.match(/[\d+$]/)
+function isProfessor(email: string) {
+  var hasDomain = /@arc.losrios.edu/.test(email);
+  var hasNumbers = /\d+/.test(email);
+  if(hasDomain && !hasNumbers){
+      console.log(`${email} is a Professor.`)
+  }
+  else{
+      console.log(`${email} is not a valid email.`)
+  }
 }
-
-isAuthorized
+//still needs to email a code; also needs a popup that blocks
+//webApp usage
